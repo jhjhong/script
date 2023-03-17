e969289d5080b0a210071b225addbdae38321334
jhjhong <jhihua.tw@gmail.com>
2023-03-17 16:26:05 +0800

    record.seq.upper()

--- rmInvalidFasta/rmInvalid.py
+++ rmInvalidFasta/rmInvalid.py
@@ -15,6 +15,8 @@ sequences = []
 
 # 解析FASTA文件，遍歷每個序列
 for record in SeqIO.parse(sys.argv[1], "fasta"):
+
+    record.seq = record.seq.upper()
     # 檢查序列中的每個鹼基是否屬於合法的字源
     if all(base in valid_aa for base in record.seq):
         # 如果是，將該序列添加到新的序列列表中
